7cbe1d5f57f94e8f99c31f9da7378e338bc05447
Гафаров Альберт Эльдарович <ag3d@mail.ru>
2023-10-17 20:06:16 +0300

    add cron

--- BetService/src/main/java/ru/gafarov/betservice/repository/BetRepository.java
+++ BetService/src/main/java/ru/gafarov/betservice/repository/BetRepository.java
@@ -12,7 +12,7 @@ public interface BetRepository extends JpaRepository<Bet, Long> {
     @Query(value = "SELECT * FROM betbot.bets where (initiator_id = ?1 or opponent_id = ?1) and status = 'ACTIVE'", nativeQuery = true)
     List<Bet> getActiveBets(long userId);
 
-    @Query(value = "SELECT * FROM betbot.bets where status = 'ACTIVE' AND finish_date < ?1 AND id NOT IN (SELECT id FROM betbot.notify_expired_status where notify_status = 'NOTIFIED')"
+    @Query(value = "SELECT * FROM betbot.bets where status = 'ACTIVE' AND finish_date < ?1 AND id NOT IN (SELECT bet_id FROM betbot.notify_expired_status where notify_status = 'NOTIFIED')"
             , nativeQuery = true)
     List<Bet> getExpiredBets(LocalDateTime localDateTime);
 }
